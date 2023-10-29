fc329c2195b2f9b5ae8fa121d16fc47350b25b98
Semen Tartyshev <s.tartyshev@yandex.ru>
2023-10-29 15:02:27 +0700

    Реализована структура приложения для первой задачи. Добавлен класс первой задачи. Добавлены два вспомогательных класса (ConsoleUI FormattedOutput).

---
+++ main.py
@@ -0,0 +1,33 @@
+from console_ui import ConsoleUI
+from number_of_common_numbers import NumberOfCommonNumbers as Task1
+from os import system
+from error_output import FormattedOutput as FOutput
+
+
+def main():
+    """
+    Функция старта главного меню.
+    """
+    while True:
+        task_selection_item = ConsoleUI.menu()
+
+        match task_selection_item:
+            case 1:
+                Task1.ui_for_task()
+
+            case 2:
+                pass
+
+            case 3:
+                pass
+
+            case 4:
+                system('CLS')
+                return
+
+            case _:
+                FOutput.error_message()
+
+
+if __name__ == '__main__':
+    main()
